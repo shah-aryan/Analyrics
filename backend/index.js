9274c264e0bccdca8ac44daeab56963da177b7fc
shaharyshahary <133163029+shah4ry@users.noreply.github.com>
2024-06-27 15:17:27 -0400

    added require path import lets see if this fixes search error

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import { PORT, mongoDBURL } from "./config.js";
 import cors from "cors";
+import path from 'path';
 
 const app = express();
 app.use(express.json());
+
 // app.use(express.static(path.join(__dirname, 'client/build')));
 
 // const corsOptions = {
