093c9f6ce0443ec9af4b084b237be074175d9944
shaharyshahary <133163029+shah4ry@users.noreply.github.com>
2024-06-28 10:12:06 -0400

    improvements to about me

--- frontend/src/components/aboutme.jsx
+++ frontend/src/components/aboutme.jsx
@@ -4,19 +4,19 @@ const AboutMeModal = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="modal modal-open">
-      <div className="modal-box p-4 max-w-96">
+    <div className="modal modal-open rounded-3xl">
+      <div className="modal-box p-4">
         <h2 className="text-lg">Hello!</h2>
         <p className="py-4 font-light">
-          My name is Aryan, and I am a full-stack developer, and a Computer Science + BBA student at the University of Michigan. I built this project using React, Express, and MongoDB to show people the beautiful insights in analyzing music.
+          My name is Aryan. I am a full-stack developer, and a Computer Science + BBA student at the University of Michigan. I built this project using React, Express, and MongoDB to show people the beautiful insights in analyzing music.
         </p>
         <p className="py-4 font-light">
-          I am passionate about software development, and I am always looking for new opportunities to learn and grow. I am currently seeking a software engineering internship for Summer 2025, and I would love to connect with you! Feel free to reach out to me at <a href="mailto:shahary@umich.edu" className="text-accent">shahary@umich.edu</a>.
+          I am currently seeking a software engineering internship for Summer 2025, and I would love to connect with you! Feel free to reach out to me at <a href="mailto:shahary@umich.edu" className="text-accent">shahary@umich.edu</a>.
         </p>
         <p className="py-4 font-light">
           For those who would like their music featured on this website, please contact me at <a href="mailto:analyrics.contact@gmail.com" className="text-accent">analyrics.contact@gmail.com</a>
         </p>
-        <p className="py-4 font-light">
+        <p className="pt-4 font-light">
           If you would like to support this project, its growth, and more like it, please buy me a coffee at <a href="https://buymeacoffee.com/shahary" className="text-accent" target="_blank" rel="noopener noreferrer">buymeacoffee.com/shahary</a>
         </p>
 
